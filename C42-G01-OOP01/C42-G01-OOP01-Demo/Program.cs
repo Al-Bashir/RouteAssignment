db02b0a7cea930ca8a3c06353ec76af73d6e0640
Mohamed Al-Bashir <mohamedal.bashir.mab@gmail.com>
2024-07-09 00:45:53 +0300

    C42_G01_OOP01_Demo

---
+++ C42-G01-OOP01/C42-G01-OOP01-Demo/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace C42_G01_OOP01_Demo
+{
+    internal class Program
+    {
+        #region Demo #6
+        static void DoSomeProtectiveCode()
+        {
+            int x, y, z;
+            bool flag;
+            do
+            {
+                Console.WriteLine("Please enter first number");
+                flag = int.TryParse(Console.ReadLine(), out x);
+            } while (!flag);
+            do
+            {
+                Console.WriteLine("Please enter second number");
+                flag = int.TryParse(Console.ReadLine(), out y);
+            } while (!flag || y == 0);
+
+            z = x / y;
+
+            int[] Numbers = { 1, 2, 3 };
+            if (Numbers?.Length > 10)
+                Numbers[10] = 100;
+        }
+        static void DoSomeCode()
+        {
+            int x, y, z;
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+            z = x / y;
+
+            int[] Numbers = { 1, 2, 3 };
+            Numbers[10] = 100;
+        }
+        static void Main()
+        {
+            try
+            {
+                DoSomeProtectiveCode();
+                throw new Exception();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                //Free unmanaged resources 
+                //Used wit connection with database
+                Console.WriteLine("Finally");
+            }
+        }
+        #endregion
+    }
+}
---
+++ C42-G01-OOP01/Common/TypeB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    struct MyStruct
+    {
+
+    }
+    internal class TypeB
+    {
+        public TypeB()
+        {
+            TypeA obj = new TypeA();
+        }
+    }
+}
