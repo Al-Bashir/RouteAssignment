c0998db8dacbdd1db301e3a8a00779fad951970f
Mohamed Al-Bashir <mohamedal.bashir.mab@gmail.com>
2024-09-25 13:33:54 +0300

    #

---
+++ C42-G01-MVC01#Demo/C42-G01-MVC01#Demo/Controllers/MoviesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace C42_G01_MVC01_Demo.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public void GetMovie() 
+        {
+        
+        }
+
+        public string Index()
+        {
+            return "Hello X, From Index";
+        }
+    }
+}
